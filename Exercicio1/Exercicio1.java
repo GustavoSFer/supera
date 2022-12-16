bde79d0d412c26d2b83e8ecc23286a91047c3e1f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-12-16 12:33:38 -0300

    Filtrando e imprimindo

--- Exercicio1/Exercicio1.java
+++ Exercicio1/Exercicio1.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Exercicio1 {
     public static void main(String[] args) throws IOException {
@@ -19,6 +21,16 @@ public class Exercicio1 {
             st = new StringTokenizer(br.readLine());
             listaNumeros.add(Integer.parseInt(st.nextToken()));
         }
-       
+        // Filtrando numeros pares e impares e colocando em ordem que deseja
+        var pares = listaNumeros.stream().filter(l -> l % 2 == 0).sorted().collect(Collectors.toList());
+        var impares = listaNumeros.stream().filter(l -> l % 2 != 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        
+        // Adicionando os valores em um novo array
+        List<Integer> total = new ArrayList<>();
+        total.addAll(pares);
+        total.addAll(impares);
+
+        //  imprimindo as informações do array
+        total.forEach(System.out::println);
     }
 }
